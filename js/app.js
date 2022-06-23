97cb9c4802c5a7548a2011e918abf7be18a0c7d7
kp_kriz <kpkriz007@gmail.com>
2022-06-23 17:05:57 +0630

    6/23/2022

--- js/app.js
+++ js/app.js
@@ -2,6 +2,31 @@
 
 $(document).ready(function(){
 
-	
+	// Start Header
+
+		// Start nav
+
+			// for bugger
+			$(".navbuttons").click(function(){
+				$(".navbuttons").toggleClass("crossxs");
+			});
+
+			// fot nav
+			$(window).scroll(function(){
+
+				let getscrolltop = $(this).scrollTop();
+				console.log(getscrolltop);
+
+				if(getscrolltop >= 200){
+					$(".navbar").addClass("navmenus");
+				}else{
+					$(".navbar").removeClass("navmenus");
+				}
+
+				});
+
+		// End nav
+
+	// End Header
 
 });
